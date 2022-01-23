11e83118be14d37ee22487882edd4357c3566822
Johan Bové <johan@johanbove.info>
2022-01-23 23:23:15 +0100

    Adds CONTRACT_TITLE check

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const DB_PUBKEY =
   "76fd126565c8bcff78fb33c2c6d1693f5b3d2b95153afd9bf8d6a704c5b53a5f";
 const ADMIN =
   "b3831fdfc16c093faef09dff4c98cf66711e1fdb8e0fe1745d9c71ff513e3831";
+  const CONTRACT_TITLE = 'Key/Value store with admin';
 
 const db = await Database.load(STORAGE, DB_PUBKEY);
-console.log("DB connected", db.pubkey.toString("hex"));
+console.log("DB loaded", db.pubkey.toString("hex"));
 
 await db.swarm(); // share on the hypercore network
 
-// await db.syncLatest();
+// await db.whenConnected();
 
 console.log("isSwarming", db.isSwarming);
 console.log("isExecutor", db.isExecutor);
@@ -22,23 +23,29 @@ console.log("executor", db.executor);
 console.log("numPeers", db.numPeers);
 console.log("oplogs", db.oplogs);
 console.log("index", db.index);
-console.log("_readContractCode", await db._readContractCode())
+
+const contractCode = await db._readContractCode();
+console.log("Expected Contract:", contractCode.indexOf(CONTRACT_TITLE) !== -1 ? `Yes, found "${CONTRACT_TITLE}".` : 'Nope... better check the contract code.');
 
 const tx0 = await db.call("getAdmin");
-console.log(tx0.response && tx0.response.value.pubkey === ADMIN ? 'Admin OK' : 'Unexpected Admin!'); // => ADMIN
+console.log(
+  tx0.response && tx0.response.value.pubkey === ADMIN
+    ? "Admin OK"
+    : "Unexpected Admin!"
+); // => ADMIN
 
 const tx1 = await db.call("get", { key: "Hello" });
 // console.log(tx1.response)
-console.log('STATE', tx1.response.name, tx1.response.value); // => Hello World
+console.log("STATE", tx1.response.name, tx1.response.value); // => Hello World
 
 // const verifyResults = await db.verify();
 // console.log("verify", verifyResults);
 
-console.log('Monitoring transactions...')
-const mon = await db.monitor() // persistently monitor transactions
-mon.on('violation', (stream) => console.log('violation', stream))
-mon.on('validated', (stream) => console.log('validated', stream))
-mon.on('added', (stream) => console.log('added', stream))
-mon.on('changed', (stream) => console.log('changed', stream))
+console.log("Monitoring transactions...");
+const mon = await db.monitor(); // persistently monitor transactions
+mon.on("violation", (stream) => console.log("violation", stream));
+mon.on("validated", (stream) => console.log("validated", stream));
+mon.on("added", (stream) => console.log("added", stream));
+mon.on("changed", (stream) => console.log("changed", stream));
 
 // db.unswarm();
